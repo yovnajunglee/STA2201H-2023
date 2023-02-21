e639ff56fb4d9152361793f835494d85429898a4
yovna26 <yovna.junglee@gmail.com>
2023-02-21 15:50:34 -0500

    updated lab5

---
+++ Labs/simple_weight_3.R
@@ -0,0 +1,34 @@
+/* Simple linear regression */
+  data {
+    int<lower=1> N;       // number of observations
+    vector[N] log_gest;    // 
+      vector[N] log_weight;     // 
+      vector[N] preterm;
+    vector[N] log_gest_preterm;
+    vector[N] sex;
+    vector[N] sex_preterm;
+  }
+parameters {
+  vector[4] beta;           // coefs
+  real<lower=0> sigma;  // error sd for Gaussian likelihood
+}
+model {
+  // Log-likelihood
+  target += normal_lpdf(log_weight | beta[1] + beta[2] * log_gest + 
+                          beta[3] * preterm + beta[4] * log_gest_preterm +
+                          beta[5] * sex + beta[6] * preterm, sigma);
+  
+  // Log-priors
+  target += normal_lpdf(sigma | 0, 1)
+  + normal_lpdf(beta | 0, 1);
+}
+generated quantities {
+  vector[N] log_lik;    // pointwise log-likelihood for LOO
+  vector[N] log_weight_rep; // replications from posterior predictive dist
+  
+  for (n in 1:N) {
+    real log_weight_hat_n = beta[1] + beta[2] * log_gest[n];
+    log_lik[n] = normal_lpdf(log_weight[n] | log_weight_hat_n, sigma);
+    log_weight_rep[n] = normal_rng(log_weight_hat_n, sigma);
+  }
+}
